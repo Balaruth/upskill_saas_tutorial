ecb25987ca288dcf350daaca85dda2fcb83ef438
Grant Donoghue <g.donoghue@outlook.com>
2017-11-11 21:34:37 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP:PORT'],
+  :address        => ENV['MAILGUN_SMTP:SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP:LOGIN'],
+  :password       => ENV['MAILGUN_SMTP:PASSWORD'],
+  :domain         => 'enigmatic-harbor-35040.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
